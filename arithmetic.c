8edcba1596bc29352fb97a278955e9226c08f24b
Naimul Islam Nahid <islamnaimul000@gmail.com>
2026-04-09 11:59:38 +0600

    module-2 of c language

---
+++ arithmetic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a=30;
+    int b=18;
+    int sum= a+b;
+    int multi= a*b;
+    float div= a/b;
+    int modu= a%b;
+    printf("SUM=%d\n multi=%d\n div=%.2f\n modu=%d\n",sum,multi,div,modu);
+}
---
+++ if_else.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if(tk>=100){
+        printf("burger");
+    }
+    else if(tk>=50){
+                printf("muri");
+
+    }
+    else{
+                printf("kisuu na ");
+
+    }
+}
